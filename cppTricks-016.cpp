9773542a68d6300b74cfa78751c15ff38c300cee
aliiashour <68806155+aliiashour@users.noreply.github.com>
2020-12-19 11:38:37 +0200

    little hard reverse number digits problem

---
+++ cppTricks-016.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+
+	/*
+		Read an integer N, then find its reverse integer R
+		Input: 123
+		Output: 321
+	 */
+	int n; cin >> n;
+	int res = 0;
+	while (n)
+	{
+		int last_digit = n % 10;
+		res = res * 10 + last_digit;
+		n /= 10;
+	}
+
+	cout << res << endl;
+
+	return 0;
+}
