dd70575c1c3b9bb572509ebb4ff69f6c22f7f993
aliiashour <68806155+aliiashour@users.noreply.github.com>
2020-12-19 09:50:46 +0200

    little hard drawing shapes with loops problem

---
+++ cppTricks-012.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+	/*
+		write a program with while loop to read an integer number
+		--in-- 4
+		--op-- 
+		*
+		**
+		***
+		****
+	*/
+	int n;cin >> n;
+	int row = n;
+	while (row > 0)
+	{
+		int c = row; 
+		while (c <= n)
+		{
+			cout << "*";
+			c++;
+		}
+		cout << endl;
+		row--;
+	}
+}
